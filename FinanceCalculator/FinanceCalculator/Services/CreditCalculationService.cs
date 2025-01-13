c0c2a0007a1c8653937f672c4aacfa686b5bc3bc
donikamarkowa <donimarkova@gmail.com>
2025-01-13 16:55:04 +0200

    Created ICreditCalculationService interface and CreditCalculationService implementation for credit calculations (monthly payment, total payment, total interest)

---
+++ FinanceCalculator/FinanceCalculator/Services/CreditCalculationService.cs
@@ -0,0 +1,26 @@
+﻿namespace FinanceCalculator.Services
+{
+    public class CreditCalculationService : ICreditCalculationService
+    {
+        // Calculate monthly payment
+        public decimal CalculateMonthlyPayment(decimal amount, decimal interestRate, int months)
+        {
+            decimal monthlyInterestRate = interestRate / 100 / 12; // monthly interest rate
+            decimal denominator = (decimal)Math.Pow((double)(1 + monthlyInterestRate), months) - 1;
+            decimal monthlyPayment = amount * monthlyInterestRate / (1 - (decimal)Math.Pow((double)(1 + monthlyInterestRate), -months));
+            return monthlyPayment;
+        }
+
+        // Calculate total payment
+        public decimal CalculateTotalPayment(decimal monthlyPayment, int months)
+        {
+            return monthlyPayment * months;
+        }
+
+        // Calculate Total Interest
+        public decimal CalculateTotalInterest(decimal totalPayment, decimal amount)
+        {
+            return totalPayment - amount;
+        }
+    }
+}
---
+++ FinanceCalculator/FinanceCalculator/Services/ICreditCalculationService.cs
@@ -0,0 +1,9 @@
+﻿namespace FinanceCalculator.Services
+{
+    public interface ICreditCalculationService
+    {
+        public decimal CalculateMonthlyPayment(decimal amount, decimal interestRate, int months);
+        public decimal CalculateTotalPayment(decimal monthlyPayment, int months);
+        public decimal CalculateTotalInterest(decimal totalPayment, decimal amount);
+    }
+}
