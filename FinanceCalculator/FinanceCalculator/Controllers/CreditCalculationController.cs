500b88070dc13c44bfb230575b3e02d925ab8af4
donikamarkowa <donimarkova@gmail.com>
2025-01-13 16:57:27 +0200

    Added CreditCalculationController to handle credit calculation logic

---
+++ FinanceCalculator/FinanceCalculator/Controllers/CreditCalculationController.cs
@@ -0,0 +1,56 @@
+﻿using FinanceCalculator.Data;
+using FinanceCalculator.Entities;
+using FinanceCalculator.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceCalculator.Controllers
+{
+    public class CreditCalculationController : Controller
+    {
+        private readonly ICreditCalculationService _creditCalculationService;
+        private readonly FinanceCalculatorDbContext _context;
+
+        // Инжектиране на зависимостите
+        public CreditCalculationController(ICreditCalculationService creditCalculationService, FinanceCalculatorDbContext context)
+        {
+            _creditCalculationService = creditCalculationService;
+            _context = context;
+        }
+
+        // Действие за показване на формата за въвеждане на данни
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Действие за изчисляване на месечно плащане, общо плащане и лихви
+        [HttpPost]
+        public async Task<IActionResult> Calculate(Credit credit)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index");
+            }
+
+            try
+            {
+                // Изчисления
+                credit.MonthlyPayment = _creditCalculationService.CalculateMonthlyPayment(credit.Amount, credit.InterestRate, credit.DurationMonths);
+                credit.TotalPayment = _creditCalculationService.CalculateTotalPayment(credit.MonthlyPayment, credit.DurationMonths);
+                credit.TotalInterest = _creditCalculationService.CalculateTotalInterest(credit.TotalPayment, credit.Amount);
+
+                // Запазване в базата
+                _context.Credits.Add(credit);
+                await _context.SaveChangesAsync();
+
+                return View("Result", credit);
+            }
+            catch (Exception)
+            {
+                // Обработка на грешки
+                ModelState.AddModelError("", "An error occurred while processing your request.");
+                return View("Index");
+            }
+        }
+    }
+}
