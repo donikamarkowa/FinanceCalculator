4bc0394a659db9ee6d62c8b2c47916aeab82fae8
donikamarkowa <donimarkova@gmail.com>
2025-01-14 20:06:52 +0200

    Add new migration and update database

--- FinanceCalculator/FinanceCalculator/Migrations/20250114180554_AddLeasingEntity.Designer.cs
+++ FinanceCalculator/FinanceCalculator/Migrations/20250114180554_AddLeasingEntity.Designer.cs
@@ -3,6 +3,7 @@ using FinanceCalculator.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FinanceCalculator.Migrations
 {
     [DbContext(typeof(FinanceCalculatorDbContext))]
-    partial class FinanceCalculatorDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250114180554_AddLeasingEntity")]
+    partial class AddLeasingEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FinanceCalculator/FinanceCalculator/Migrations/20250114180554_AddLeasingEntity.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanceCalculator.Migrations
+{
+    public partial class AddLeasingEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Leasings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LeasingAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    InterestRate = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DurationMonths = table.Column<int>(type: "int", nullable: false),
+                    DownPayment = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    MonthlyPayment = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TotalPayment = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TotalInterest = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Leasings", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Leasings");
+        }
+    }
+}
