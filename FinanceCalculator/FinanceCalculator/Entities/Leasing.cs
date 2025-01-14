7d5c014524a1c65899a638b8ac0b8d7a494a15f6
donikamarkowa <donimarkova@gmail.com>
2025-01-14 20:06:24 +0200

    Add new entity for leasing calculator

---
+++ FinanceCalculator/FinanceCalculator/Entities/Leasing.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinanceCalculator.Entities
+{
+    public class Leasing
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal LeasingAmount { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal InterestRate { get; set; }
+
+        [Required]
+        public int DurationMonths { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal DownPayment { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal MonthlyPayment { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal TotalPayment { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal TotalInterest { get; set; }
+    }
+}
