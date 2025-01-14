14de1dede9d903698b756749acd8316e648f96de
donikamarkowa <donimarkova@gmail.com>
2025-01-14 20:13:42 +0200

    Add new service and interface

---
+++ FinanceCalculator/FinanceCalculator/Services/ILeasingCalculationService.cs
@@ -0,0 +1,9 @@
+﻿namespace FinanceCalculator.Services
+{
+    public interface ILeasingCalculationService
+    {
+        decimal CalculateMonthlyPayment(decimal leasingAmount, decimal interestRate, int durationMonths, decimal downPayment);
+        decimal CalculateTotalPayment(decimal monthlyPayment, int durationMonths);
+        decimal CalculateTotalInterest(decimal totalPayment, decimal leasingAmount, decimal downPayment);
+    }
+}
---
+++ FinanceCalculator/FinanceCalculator/Services/LeasingCalculationService.cs
@@ -0,0 +1,24 @@
+﻿namespace FinanceCalculator.Services
+{
+    public class LeasingCalculationService : ILeasingCalculationService
+    {
+        public decimal CalculateMonthlyPayment(decimal leasingAmount, decimal interestRate, int durationMonths, decimal downPayment)
+        {
+            decimal principal = leasingAmount - downPayment;
+            decimal monthlyInterestRate = interestRate / 100 / 12;
+            decimal monthlyPayment = principal * monthlyInterestRate /
+                (1 - (decimal)Math.Pow((double)(1 + monthlyInterestRate), -durationMonths));
+            return monthlyPayment;
+        }
+
+        public decimal CalculateTotalPayment(decimal monthlyPayment, int durationMonths)
+        {
+            return monthlyPayment * durationMonths;
+        }
+
+        public decimal CalculateTotalInterest(decimal totalPayment, decimal leasingAmount, decimal downPayment)
+        {
+            return totalPayment - (leasingAmount - downPayment);
+        }
+    }
+}
