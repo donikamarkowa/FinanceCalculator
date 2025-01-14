33c649f787786020bf3096e53cc43c7d79e3a2dc
donikamarkowa <donimarkova@gmail.com>
2025-01-14 18:34:14 +0200

    Add new service and interface with methods for refinancing calculator

---
+++ FinanceCalculator/FinanceCalculator/Services/IRefinancingCalculationService.cs
@@ -0,0 +1,9 @@
+﻿namespace FinanceCalculator.Services
+{
+    public interface IRefinancingCalculationService
+    {
+        decimal CalculateMonthlyPayment(decimal loanAmount, decimal interestRate, int durationMonths);
+        decimal CalculateTotalPayment(decimal monthlyPayment, int durationMonths);
+        decimal CalculateSavings(decimal currentLoanAmount, decimal totalNewPayment);
+    }
+}
---
+++ FinanceCalculator/FinanceCalculator/Services/RefinancingCalculationService.cs
@@ -0,0 +1,21 @@
+﻿namespace FinanceCalculator.Services
+{
+    public class RefinancingCalculationService : IRefinancingCalculationService
+    {
+        public decimal CalculateMonthlyPayment(decimal loanAmount, decimal interestRate, int durationMonths)
+        {
+            var monthlyRate = interestRate / 100 / 12;
+            return loanAmount * monthlyRate / (1 - (decimal)Math.Pow((double)(1 + monthlyRate), -durationMonths));
+        }
+
+        public decimal CalculateTotalPayment(decimal monthlyPayment, int durationMonths)
+        {
+            return monthlyPayment * durationMonths;
+        }
+
+        public decimal CalculateSavings(decimal currentLoanAmount, decimal totalNewPayment)
+        {
+            return currentLoanAmount - totalNewPayment;
+        }
+    }
+}
